fe352208f4e51c639bb435a1b2368df9b1416381
Andreas Lochwitz <andreas.lochwitz@live.de>
2019-12-18 16:54:10 +0100

    Added files

--- script.py
+++ script.py
@@ -1,1 +1,38 @@
+from util.TextBuffer import TextBuffer
+
 print("Hello, World!")
+
+f = open('data/Twine2Test.html')
+content = f.read()
+f.close()
+
+t = TextBuffer(content)
+r = t.get_text_part("<tw-storydata", "</tw-storydata")
+print(r)
+t.set_text(r)
+t.reset()
+r = t.get_text_part("tw-passagedata", ">")
+print(r)
+print(t.get_parameter_data(r, "name"))
+print(t.get_parameter_data(r, "pid"))
+print(t.get_parameter_data(r, "tags"))
+
+t.reset()
+
+result = t.get_text_part("<tw-passagedata", "</tw-passagedata>")
+z = 1
+while result is not False:
+    print("z = " + str(z))
+    print(result)
+    # temp = TextBuffer(result)
+    scene_content = TextBuffer.get_text_til_end(result, "\">")
+    temp = TextBuffer(scene_content)
+    link_result = temp.get_text_part("[[", "]]")
+    while link_result is not False:
+        print(link_result)
+        temp.replace_string("[[" + link_result + "]]", "")
+        link_result = temp.get_text_part("[[", "]]")
+        print(temp.get_text())
+    z = z + 1
+    result = t.get_text_part("<tw-passagedata", "</tw-passagedata>")
+
--- util/TextBuffer.py
+++ util/TextBuffer.py
@@ -13,3 +13,32 @@ class TextBuffer:
                 self.search_pos = end_pos + 1
                 return self.text_content[start_pos + len(start_part):end_pos]
         return False
+
+    def reset(self):
+        self.search_pos = 0
+
+    def set_text(self, text):
+        self.text_content = text
+
+    def get_text(self):
+        return self.text_content
+
+    @staticmethod
+    def get_parameter_data(text, parametername):
+        start_pos = text.find(parametername + "=\"")
+        if start_pos > -1:
+            end_pos = text.find("\"", start_pos + len(parametername) +2)
+            if end_pos > -1:
+                return text[start_pos + len(parametername) + 2:end_pos]
+        return False
+
+    @staticmethod
+    def get_text_til_end(text, parameter_name):
+        start_pos = text.find(parameter_name)
+        if start_pos > -1:
+            return text[start_pos + len(parameter_name):]
+        return False
+
+    def replace_string(self, old_string, new_string):
+        self.text_content = self.text_content.replace(old_string, new_string, 1)
+        self.reset()
